b73daec589f6dc50604ae41a3c55814b8f4c8b9d
Travis Mathis <travisdmathis@gmail.com>
2020-07-14 10:15:10 -0400

    updates

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,6 +2,8 @@
   import { onMount } from "svelte";
   import LandingPage from "../components/LandingPage.svelte";
 
+  let bamboo = "./images/bamboo.svg";
+
   onMount(() => {
     for (let i = 0; i < 100; i++) {
       let star =
@@ -47,17 +49,19 @@
   <h1
     class="text-base sm:text-xl md:text-2xl lg:text-3xl my-auto mx-auto md:mx-48
     ">
-    Sleep better at night knowing your assets are covered.
+    Sleep better at night knowing your assets are covered
     <br />
     <span class="brand-green-dark text-base sm:text-lg md:text-xl lg:text-2xl">
       Decentralized Management of Insurance Liquidity Pools
     </span>
     <br />
-    <button href="#about-dao" class="button-brand-grey mt-5 font-semibold">
-      learn about the dao
+    <button
+      href="#about-dao"
+      class="flex flex-row justify-between button-brand-grey mt-5 font-semibold">
+      <img src={bamboo} alt="Join the BAMBOO" class="w-8 bamboo-icon" />
+      Learn about the DAO
     </button>
   </h1>
-
 </section>
 
 <LandingPage />
